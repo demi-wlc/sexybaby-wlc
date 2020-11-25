4be722dc6d9c38d3da3951f358c7f5219a5dd37b
demi <1561955658@qq.com>
2020-11-25 16:14:44 +0800

    project commit

---
+++ src/main/java/edu/nf/wlc/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package edu.nf.wlc.dao;
+
+import edu.nf.wlc.entity.Product;
+
+public class ProductDao {
+    public void save(Product product){
+
+    }
+}
---
+++ src/main/java/edu/nf/wlc/entity/Product.java
@@ -0,0 +1,8 @@
+package edu.nf.wlc.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+}
